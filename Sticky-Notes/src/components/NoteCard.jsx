8b99d73892891fa4fc90fc7fe64316a28fbf4f1f
Allison <Allison.Boone@keyin.com>
2024-10-18 21:16:43 -0230

    Added functionality to the sticky notes so now they are able to be moved around

--- Sticky-Notes/src/components/NoteCard.jsx
+++ Sticky-Notes/src/components/NoteCard.jsx
@@ -1,11 +1,13 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import Trash from '../assets/icons/Trash';
 
 const NoteCard = ({ note }) => {
   const body = JSON.parse(note.body);
-  const position = JSON.parse(note.position);
+  const [position, setPositon] = useState(JSON.parse(note.position));
   const colors = JSON.parse(note.colors);
 
+  let mouseStartPos = { x: 0, y: 0 };
+  const cardRef = useRef(null);
   const textAreaRef = useRef(null);
 
   useEffect(() => {
@@ -19,8 +21,37 @@ const NoteCard = ({ note }) => {
     current.style.height = current.scrollHeight + 'px';
   };
 
+  const mouseDown = (e) => {
+    mouseStartPos.x = e.clientX;
+    mouseStartPos.y = e.clientY;
+
+    document.addEventListener('mousemove', mouseMove);
+    document.addEventListener('mouseup', mouseUp);
+  };
+
+  const mouseMove = (e) => {
+    let mouseMoveDir = {
+      x: mouseStartPos.x - e.clientX,
+      y: mouseStartPos.y - e.clientY,
+    };
+
+    mouseStartPos.x = e.clientX;
+    mouseStartPos.y = e.clientY;
+
+    setPositon({
+      x: cardRef.current.offsetLeft - mouseMoveDir.x,
+      y: cardRef.current.offsetTop - mouseMoveDir.y,
+    });
+  };
+
+  const mouseUp = () => {
+    document.removeEventListener('mousemove', mouseMove);
+    document.removeEventListener('mouseup', mouseUp);
+  };
+
   return (
     <div
+      ref={cardRef}
       className="card"
       style={{
         backgroundColor: colors.colorBody,
@@ -29,6 +60,7 @@ const NoteCard = ({ note }) => {
       }}
     >
       <div
+        onMouseDown={mouseDown}
         className="card-header"
         style={{ backgroundColor: colors.colorHeader }}
       >
